557c5c1c726f8406d21ca8c009979b4b075d9176
MarkJerielCabalbag <cabalbagmarkjeriel03@gmail.com>
2024-02-09 19:54:41 +0800

    new nav layout

--- src/components/navigation/Dashboard.jsx
+++ src/components/navigation/Dashboard.jsx
@@ -1,3 +1,11 @@
+import {
+  faBook,
+  faHome,
+  faUserAlt,
+  faUserAltSlash,
+  faUserCheck,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { Link, Outlet } from "react-router-dom";
 
@@ -6,7 +14,11 @@ const Dashboard = () => {
     <>
       <nav className="navbar navbar-expand-lg bg-white sticky-top shadow p-3 mb-5">
         <div className="container-sm">
-          <img src="src/assets/paesano_logo.jpg" style={{ width: "50px" }} />
+          <img
+            src="src/assets/paesano_logo.jpg"
+            style={{ width: "50px" }}
+            className="me-5"
+          />
           <button
             className="navbar-toggler"
             type="button"
@@ -20,10 +32,22 @@ const Dashboard = () => {
           </button>
           <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div className="navbar-nav gap-3">
-              <Link to="home">Home</Link>
-              <Link to="book">Book</Link>
-              <Link to="account">Account</Link>
-              <Link to="creator">Creator</Link>
+              <Link className="link link-secondary" to="home">
+                <FontAwesomeIcon icon={faHome} /> Home
+              </Link>
+              <Link className="link link-secondary" to="book">
+                <FontAwesomeIcon icon={faBook} /> Book
+              </Link>
+
+              <Link className="link link-secondary" to="creator">
+                <FontAwesomeIcon icon={faUserCheck} />
+                Creator
+              </Link>
+
+              <Link className="link link-secondary" to="account">
+                <FontAwesomeIcon icon={faUserAlt} />
+                Account
+              </Link>
             </div>
           </div>
         </div>
